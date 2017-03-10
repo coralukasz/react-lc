d44c287aa1b3fc574f6db16187c3670b88b69cda
CORA Lukasz <B3J@dsone.3ds.com>
2017-03-10 10:12:55 +0100

    changes

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
 	render() {
+		const postId = this.props.params.postId;
+		//index of the post
+		const i = this.props.posts.findIndex( (post) => post.code === postId );
+		// get the post
+		const post = this.props.posts[i];
+		const postComments = this.props.comments[postId] || [];
 		return (
 			<div className="single-photo">
-				single
+				<Photo i={i} post={post} {...this.props} />
+				<Comments  postComments={ postComments } {...this.props} />
 			</div>
 		)
 	}
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,8 +2,33 @@
 // 1 the action 
 // 2 copy of current state
 
+function postComments(state = [], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+			return [...state,{
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			console.log('rem com');
+			return [
+				...state.slice(0,action.i),
+				...state.slice(action.i + 1)
+			];
+		default:
+			return state;
+	}
+	return state;
+}
+
 function comments(state = [], action) {
-	console.log(state, action);
+	console.log(action.postId);
+	if ( typeof action.postId !== 'undefined' ) {
+		return {
+			...state,
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
 	return state;
 }
 
